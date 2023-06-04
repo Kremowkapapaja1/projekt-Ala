b3d5ac6eee72f5b7b1e80d6ba89e94d9481c90c0
Kremowkapapaja1 <radoslaw.antoni.czerwinski@gmail.com>
2023-06-04 20:46:49 +0200

    test

--- projekt.py
+++ projekt.py
@@ -6,5 +6,11 @@ from matplotlib.animation import FuncAnimation
 
 total_time = 10 #czas trwania animacji w sekundach
 delta_t = 0.1   #Krok czasowy w sekundach
-A = 0 #odległość od startu  
+s = s + 1 
+for s in range 20:
+    a=5*a
+    if a>50:
+        s-=10
+        a=50-5*s
 P = 100 #wartpść siły skupionej w Newtonach
+
