518188443fe66893babdffd5206e71529f297af4
Kremowkapapaja1 <radoslaw.antoni.czerwinski@gmail.com>
2023-06-04 20:17:56 +0200

    dane wejciowe

--- projekt.py
+++ projekt.py
@@ -1,3 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
+
+#dane wejściowe
+
+total_time = 10 #czas trwania animacji w sekundach
+delta_t = 0.1   #Krok czasowy w sekundach
+A = 10 #długość belki  
+P = 100 #wartpść siły skupionej w Newtonach
